f1c4350e994d58cbf9b5b370640d9d46a143c2b3
Yury Ivanchenko <tech.jurgenet@gmail.com>
2018-02-16 17:43:36 +0300

    Added SidebarNavigation component

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,7 +1,25 @@
-const routes = {
+export default {
   '/': 'Home',
   '/dashboard': 'Dashboard',
   '/tableset': 'Tableset',
+  items: [
+    {
+      name: 'Dashboard',
+      url: '/dashboard',
+      icon: 'icon-speedometer',
+      badge: {
+        variant: 'info',
+        text: 'NEW',
+      },
+    },
+    {
+      name: 'Tableset',
+      url: '/tableset',
+      icon: 'icon-speedometer',
+      badge: {
+        variant: 'info',
+        text: 'OLD',
+      },
+    },
+  ],
 };
-
-export default routes;
